6dd818b513cdfec417f72e3865f999210254a805
FMA Toxic <mintyoculus@gmail.com>
2025-05-29 17:12:59 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add hover effect that lights up each letter
-document.querySelectorAll('.letter').forEach(letter => {
-  letter.addEventListener('mouseenter', () => {
-    letter.classList.add('light-up');
-  });
-  letter.addEventListener('mouseleave', () => {
-    letter.classList.remove('light-up');
-  });
-});
